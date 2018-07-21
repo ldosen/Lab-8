7448a4da8e02bd7e4073fa2974c1ee4f94b74d22
Luke Dosen <lddosen@gmail.com>
2018-07-21 00:17:54 -0500

    DescendingByCount and Main completed and working as intended

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -9,15 +9,20 @@ public class Main {
     // set up the scanner so that it separates words based on space and punctuation
     final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
 
-    // TODO complete this main program
-    // 1. create a WordCounter instance
-    // 2. use this to count the words in the input
-    // 3. determine the size of the resulting map
-    // 4. create an ArrayList of that size and
-    // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
-    // 6. sort the ArrayList in descending order by count
-    //    using Collections.sort and an instance of the provided comparator (after fixing the latter)
-    // 7. print the (up to) ten most frequent words in the text
+    final Map<String, Integer> aMap = new HashMap<>();
 
+    WordCounter count = new WordCounter(aMap);
+    while (input.hasNext()){
+      count.countWords(input);
+    }
+
+    final List list =new ArrayList(count.getCounts().size());
+    list.addAll(aMap.entrySet());
+
+    Collections.sort(list, new DescendingByCount());
+
+    for(int i = 0; i < 10; i++){
+      System.out.println(list.get(i));
+    }
   }
 }
